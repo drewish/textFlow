adaf94cd3a4dc226ab1f0c5e216b4573dd112fd2
andrew morton <drewish@katherinehouse.com>
2013-08-11 23:24:19 -0700

    Switch from ncurses gem to ruby's curses

--- textFlow.rb
+++ textFlow.rb
@@ -26,25 +26,30 @@ begin;
   require 'rubygems'
   require 'osax'
   require 'appscript'
-  require 'ncurses'
+  require 'curses'
   rescue LoadError;
 end
 include Appscript
 include OSAX
 
+# Little helper to replicate ncurses' function to move and print a string.
+def mvaddstr y, x, str
+  Curses.stdscr.setpos y, x
+  Curses.stdscr.addstr str
+end
+
 begin
-  # initialize ncurses
-  Ncurses.initscr
-  Ncurses.cbreak           # provide unbuffered input
-  Ncurses.noecho           # turn off input echoing
-  Ncurses.nonl             # turn off newline translation
+  # initialize curses
+  Curses.init_screen
+  Curses.cbreak           # provide unbuffered input
+  Curses.noecho           # turn off input echoing
+  Curses.nonl             # turn off newline translation
 
-  Ncurses.stdscr.intrflush(false) # turn off flush-on-interrupt
-  Ncurses.stdscr.keypad(true)     # turn on keypad mode
+  Curses.stdscr.keypad(true)     # turn on keypad mode
 
-  Ncurses.stdscr.mvaddstr(0, 0, 'Loading iTunes...')
+  mvaddstr(0, 0, 'Loading iTunes...')
   it = app('iTunes')
-  Ncurses.stdscr.mvaddstr(1, 0, 'Reading first track...')
+  mvaddstr(1, 0, 'Reading first track...')
 
   # If iTunes hasn't played anything since starting then current_track won't be
   # defined. To avoid this we can start and the stop the player.
@@ -61,60 +66,54 @@ begin
       # Check if the track has changed and update the display.
       if lastTrackId != it.current_track.database_ID.get
         lastTrackId = it.current_track.database_ID.get
-        Ncurses.stdscr.clear
+        Curses.stdscr.clear
 
         # Convert the album art into ascii and dump it to the screen.
         if it.current_track.artworks.get.length > 0
-          cmd = 'convert - -contrast-stretch 5%x2% jpg:- | jp2a --height=' + (Ncurses.LINES()).to_s + ' -'
+          cmd = 'convert - -contrast-stretch 5%x2% jpg:- | jp2a --height=' + (Curses.lines()).to_s + ' -'
           IO.popen(cmd, 'r+') do |pipe|
             pipe << it.current_track.artworks.first.get.raw_data.get.data
             pipe.close_write
             ascii = pipe.readlines
             ascii.each_index {|i|
-              Ncurses.stdscr.mvaddstr(i, 0, ascii[i])
+              mvaddstr(i, 0, ascii[i])
             }
             artWidth = ascii.first.length
           end
         end
 
-        Ncurses.stdscr.mvaddstr(Ncurses.LINES() / 2 + 0, artWidth + 2, it.current_track.artist.get)
-        Ncurses.stdscr.mvaddstr(Ncurses.LINES() / 2 + 1, artWidth + 2, it.current_track.name.get)
-        Ncurses.refresh
+        mvaddstr(Curses.lines() / 2 + 0, artWidth + 2, it.current_track.artist.get)
+        mvaddstr(Curses.lines() / 2 + 1, artWidth + 2, it.current_track.name.get)
+        Curses.refresh
       end
     # Generally the CommandError is thrown when there's not a current track.
     rescue CommandError
-      Ncurses.stdscr.clear
-      Ncurses.stdscr.mvaddstr(Ncurses.LINES() / 2, artWidth + 2, 'Nothing playing.')
-      Ncurses.refresh
+      Curses.stdscr.clear
+      mvaddstr(Curses.lines() / 2, artWidth + 2, 'Nothing playing.')
+      Curses.refresh
       lastTrackId = nil
     end
 
     # Wait for keyboard input for half a second then give up so we can check
     # if the track has changed.
     if IO.select([STDIN], nil, nil, 0.5)
-      ch = Ncurses.stdscr.getch()
+      ch = Curses.stdscr.getch()
       case(ch)
-      when ?\ :
-        it.playpause
-      when Ncurses::KEY_RIGHT :
-        it.next_track
-      when Ncurses::KEY_LEFT :
-        it.previous_track
-      when Ncurses::KEY_UP :
-        osax.set_volume(:output_volume => 100)
-      when Ncurses::KEY_DOWN :
-        osax.set_volume(:output_volume => 0)
-      when 'q'[0], 'Q'[0] :
-        exit
-  #    else
-  #      Ncurses.stdscr.mvaddstr(3, 0, ch.to_s)
+      when ?\                then it.playpause
+      when Curses::KEY_RIGHT then it.next_track
+      when Curses::KEY_LEFT  then it.previous_track
+      when Curses::KEY_UP    then osax.set_volume(:output_volume => 100)
+      when Curses::KEY_DOWN  then osax.set_volume(:output_volume => 0)
+      when 'q'[0], 'Q'[0]    then exit
+      # For debugging it can be helpful to see the character:
+      # else mvaddstr(3, 0, ch.to_s)
       end
     end
   end while true
 
 ensure
-  Ncurses.echo
-  Ncurses.nocbreak
-  Ncurses.nl
-  Ncurses.endwin
+  Curses.echo
+  Curses.nocbreak
+  Curses.nl
+  Curses.close_screen
 end
